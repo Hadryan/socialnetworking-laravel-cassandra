48761ff16648943b3bf372cd0016c2ed4771830f
ruphin15 <ru_phin26@yahoo.com>
2018-08-13 02:02:13 +0800

    CommonFriends daemon failed to start because of Cass ANDOR Rabbit are not ready yet. So make sure those services are up first!

--- install/background_jobs/start-app.sh
+++ install/background_jobs/start-app.sh
@@ -3,10 +3,10 @@ chmod 777 -R /var/www/localhost/htdocs/bootstrap/cache
 
 crond -l 2 -f &
 
-
 php-fpm7
 
+# make sure cassandra and rabbitmq are ready before the daemon started
+sleep 25
 php /var/www/localhost/htdocs/artisan command:common-friends
 
 nginx
-
